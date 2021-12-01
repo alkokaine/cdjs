aafb6eef5ba44397eb1e88b76d4fb727c7900922
Алексей Коковин <alexey.kokovin@yandex.ru>
2021-12-01 19:16:41 +0300

    lib export

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap'
+
+import CDForm from './components/cd-form.vue'
+import CDGrid from './components/cd-grid.vue'
+import CDList from './components/cd-list.vue'
+import CDMenu from './components/cd-menu.vue'
+
+export default {
+  CDForm,
+  CDGrid,
+  CDList,
+  CDMenu
+}
