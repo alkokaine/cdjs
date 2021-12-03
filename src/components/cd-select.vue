681ac2510be8bef68cb455317497c9300b5790d3
sit19 <alkokaine@yandex.ru>
2021-12-03 06:22:22 +0300

    property (function) which resolves if option disabled, prepare to selected differentiation

--- src/components/cd-select.vue
+++ src/components/cd-select.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cd-select">
       <select>
-        <option v-for="(option) in list" :key="option[keyfield]" :value="option[keyfield]" :label="option[labelkey]"/>
+        <option v-for="(option) in list" :key="option[keyfield]" :value="option[keyfield]" :label="option[labelkey]" :disabled="isdisabled(option)"/>
       </select>
   </div>
 </template>
@@ -12,6 +12,8 @@ export default {
   name: 'cd-select',
   mixins: [collection],
   props: {
+    selected: { type: [Number, Array] },
+    isdisabled: { type: Function },
     keyfield: { type: String, required: true },
     labelkey: { type: String, required: true }
   },
