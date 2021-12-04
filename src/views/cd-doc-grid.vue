041839865a891366dc1cede37db9b6e83974014b
Алексей Коковин <alexey.kokovin@yandex.ru>
2021-12-04 07:35:46 +0300

    docs & examples: router, views

---
+++ src/views/cd-doc-grid.vue
@@ -0,0 +1,42 @@
+<template>
+  <cd-list :collection="doc" keyfield="id">
+    <div class="cd-grid--main" slot="header">
+      <span class="h3">cd-grid</span>
+    </div>
+    <div class="cd-grid--part" slot-scope="info">
+      {{ info }}
+    </div>
+  </cd-list>
+</template>
+
+<script>
+import CDList from '../components/cd-list.vue'
+export default {
+  name: 'cd-doc-grid',
+  components: {
+    'cd-list': CDList
+  },
+  data (view) {
+    return {
+      doc: [
+        {
+          id: 'grid-a',
+          header: 'AA'
+        },
+        {
+          id: 'grid-b',
+          header: 'BB'
+        },
+        {
+          id: 'grid-c',
+          header: 'CC'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/cd-doc-home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="cd-doc-home">
+      <span>Hello world</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cd-doc-home'
+}
+</script>
+
+<style>
+
+</style>
