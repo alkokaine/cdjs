b491e56965044c4e2cd02e09124a73b85744404e
sit19 <alkokaine@yandex.ru>
2021-12-04 14:14:23 +0300

    doc components, not includes to lib

---
+++ src/generic/cd-part-generic.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="cd-doc-part">
+    <h3>{{ part.header }}</h3>
+    <span v-for="(description, index) in part.description" :key="index" v-html="description"></span>
+    <span>{{ part.code }}</span><br/>
+    <cd-list :collection="part.info" keyfield="id">
+      <cd-doc-part class="cd-part-info" slot-scope="info" :part="info.row"></cd-doc-part>
+    </cd-list>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import CDList from '../components/cd-list.vue'
+export default {
+  name: 'cd-doc-part',
+  components: {
+    'cd-list': CDList
+  },
+  props: {
+    part: { type: Object, required: true }
+  }
+}
+</script>
+
+<style>
+
+</style>
