8215f161fc2806b54ca3aa9ea20763fed5223436
Алексей Коковин <alexey.kokovin@yandex.ru>
2021-12-07 15:11:31 +0300

    required & resolvedefault

--- src/generic/cd-doc-component-info.vue
+++ src/generic/cd-doc-component-info.vue
@@ -7,7 +7,7 @@
 
 <script>
 import CDGrid from '../components/cd-grid.vue'
-
+import utils from '../common/utils'
 export default {
   components: {
     'cd-grid': CDGrid
@@ -29,8 +29,16 @@ export default {
           text: 'Type'
         },
         {
+          datafield: 'required',
+          text: 'Обязательное'
+        },
+        {
           datafield: 'description',
           text: 'Описание'
+        },
+        {
+          datafield: 'default',
+          text: 'default'
         }
       ]
     }
@@ -42,7 +50,9 @@ export default {
       return {
         propertyname: propertyname,
         type: info.resolvetype(value.type),
-        description: value.description
+        description: value.description,
+        required: value.required,
+        default: info.resolvedefault(value.default)
       }
     },
     resolvetype (propertyvalue) {
@@ -53,6 +63,10 @@ export default {
       } else if (typeof propertyvalue === 'function') {
         return propertyvalue.name
       }
+    },
+    resolvedefault (check) {
+      if (typeof check === 'function') return utils.extractarguments(check)
+      return check
     }
   }
 }
