cf66c7cae67d5d64180c79e2bca83cfcab03a942
Алексей Коковин <alexey.kokovin@yandex.ru>
2021-12-07 12:00:06 +0300

    views for menu and form info

---
+++ src/views/cd-doc-menu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <cd-doc :content="content"></cd-doc>
+    <cd-info property="props" :component="menu" :key="index"></cd-info>
+  </div>
+</template>
+
+<script>
+
+import CDMenu from '../components/cd-menu.vue'
+import CDDocGeneric from '../generic/cd-doc-generic.vue'
+import ComponentInfo from '../generic/cd-doc-component-info.vue'
+
+export default {
+  components: { 'cd-doc': CDDocGeneric, 'cd-info': ComponentInfo },
+  data (doc) {
+    return {
+      menu: CDMenu,
+      content: [
+        {
+          id: 'menu-1',
+          header: 'CD-MENU'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
