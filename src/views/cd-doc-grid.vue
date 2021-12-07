e8a1b318b8f5718cc5e8a60843abd6ec908a3934
Алексей Коковин <alexey.kokovin@yandex.ru>
2021-12-07 10:43:18 +0300

    small changes (actually, preparing to describe grid)

--- src/views/cd-doc-grid.vue
+++ src/views/cd-doc-grid.vue
@@ -23,35 +23,7 @@ export default {
         {
           id: 'grid-a',
           header: 'CD-GRID',
-          description: ['aaa']
-        },
-        {
-          id: 'grid-b',
-          header: 'BB',
-          subs: [
-            {
-              id: 'grid-b-1',
-              header: 'BB-1',
-              info: [
-                {
-                  description: 'BB is BB and this is law',
-                  code: 'here will be code (soon)'
-                }
-              ]
-            },
-            {
-              id: 'grid-b-2',
-              header: 'BB-2'
-            },
-            {
-              id: 'grid-b-3',
-              header: 'BB-3'
-            }
-          ]
-        },
-        {
-          id: 'grid-c',
-          header: 'CC'
+          description: ['Что-то будет']
         }
       ]
     }
