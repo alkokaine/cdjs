b253608fe6b5640f2e18b561f6ddc60ea0ab168f
Алексей Коковин <alexey.kokovin@yandex.ru>
2021-12-07 10:17:50 +0300

    grid displays component properties (or methods, or something else)

---
+++ src/generic/cd-doc-component-info.vue
@@ -0,0 +1,35 @@
+<template>
+  <cd-grid keyfield="propertyname" :collection="data" :descriptor="descriptor"></cd-grid>
+</template>
+
+<script>
+import CDGrid from '../components/cd-grid.vue'
+
+export default {
+  components: {
+    'cd-grid': CDGrid
+  },
+  props: {
+    component: { type: Object, required: true },
+    property: { type: String, required: true }
+  },
+  data (info) {
+    return {
+      data: Array.from(Object.keys(info.component[info.property])).map(pname => ({
+        propertyname: pname,
+        value: info.component[pname]
+      })),
+      descriptor: [
+        {
+          datafield: 'propertyname',
+          text: 'PropertyName'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
