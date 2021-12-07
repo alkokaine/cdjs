73f0a7d7a5b229d2db119a553d14f42e05ffb89d
Алексей Коковин <alexey.kokovin@yandex.ru>
2021-12-07 10:37:08 +0300

    using cd-info to display grid and list properties with generic ones

--- src/views/cd-doc-grid.vue
+++ src/views/cd-doc-grid.vue
@@ -1,17 +1,24 @@
 <template>
-  <cd-doc :content="doc"></cd-doc>
+  <div>
+    <cd-doc :content="doc"></cd-doc>
+    <cd-info v-for="(info, index) in infos" :component="info" property="props" :key="index"></cd-info>
+  </div>
 </template>
 
 <script>
 import CDDocGeneric from '../generic/cd-doc-generic.vue'
+import ComponentInfo from '../generic/cd-doc-component-info.vue'
+import CDGrid from '../components/cd-grid.vue'
 
 export default {
   name: 'cd-doc-grid',
   components: {
-    'cd-doc': CDDocGeneric
+    'cd-doc': CDDocGeneric,
+    'cd-info': ComponentInfo
   },
   data (view) {
     return {
+      infos: CDGrid.mixins.concat(CDGrid),
       doc: [
         {
           id: 'grid-a',
---
+++ src/views/cd-doc-list.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <cd-doc :content="content"></cd-doc>
+    <cd-info v-for="(o, index) in infos" property="props" :component="o" :key="index"></cd-info>
+  </div>
+</template>
+
+<script>
+
+import CDList from '../components/cd-list.vue'
+import CDDocGeneric from '../generic/cd-doc-generic.vue'
+import ComponentInfo from '../generic/cd-doc-component-info.vue'
+
+export default {
+  components: { 'cd-doc': CDDocGeneric, 'cd-info': ComponentInfo },
+  data (doc) {
+    return {
+      infos: CDList.mixins.concat(CDList),
+      content: [
+        {
+          id: 'list-1',
+          header: 'Всё коллекция'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
