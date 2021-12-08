fc296990b8709e60061479daba7cb78d730b5ff7
sit19 <alkokaine@yandex.ru>
2021-12-09 00:47:41 +0300

    select property sandbox

---
+++ src/generic/cd-prop-example.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cd-prop-example">
+      <cd-form class="cd-descriptor--editor" :payload="property" :descriptor="options"></cd-form>
+      <div class="cd-descriptor--view"><code>{{ property }}</code></div>
+      <cd-form class="cd-example--form" :payload="payload" :descriptor="descriptor"></cd-form>
+  </div>
+</template>
+
+<script>
+import CDForm from '@/components/cd-form.vue'
+
+export default {
+  name: 'cd-prop-example',
+  components: {
+    'cd-form': CDForm
+  },
+  props: {
+    property: { type: Object, reqiuired: true },
+    options: { type: Array, required: true },
+    convertproperty: { type: Function },
+    payload: { type: Object, required: true }
+  },
+  data (example) {
+    return {
+      descriptor: [
+        example.convertproperty(example.property)
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  .cd-prop-example {
+    display: flex;
+  }
+  .cd-descriptor--editor {
+    width: 30%
+  }
+  .cd-descriptor--view {
+    width: 30%;
+  }
+  .cd-example--form {
+    width: 30%;
+  }
+</style>
