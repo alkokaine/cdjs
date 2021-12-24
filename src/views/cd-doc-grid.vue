bef8ecd715d9f0bf72afd7a7ee63e7cc26840152
Алексей Коковин <alexey.kokovin@yandex.ru>
2021-12-24 11:40:16 +0300

    grid examples: filter

--- src/views/cd-doc-grid.vue
+++ src/views/cd-doc-grid.vue
@@ -2,11 +2,13 @@
   <div>
     <cd-doc :content="doc"></cd-doc>
     <cd-info v-for="(info, index) in infos" :component="info" property="props" :key="index"></cd-info>
-    <cd-grid :crud="crud" :descriptor="descriptor" keyfield="id" :payload="payload" :resolveresult="resolveresult" :collection="cities" :total="total" :paging="true" :pageSize="10" :resolvepayload="resolvepayload" :onpagechange="onpagechange"></cd-grid>
+    <cd-grid :get="get" :descriptor="descriptor" keyfield="id" :payload="payload" :resolveresult="resolveresult" :collection="cities" :total="total" :paging="true" :pageSize="10" :resolvepayload="resolvepayload" :onpagechange="onpagechange"></cd-grid>
+    <cd-grid :get="get" :descriptor="descriptor" keyfield="id" :payload="payload2" :resolveresult="resolveresult2" :collection="cities2" :total="total2" :paging="true" :pageSize="10" :resolvepayload="resolvepayload" :onpagechange="onpagechange2" :usefilter="true" :filter="filter"></cd-grid>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import CDDocGeneric from '../generic/cd-doc-generic.vue'
 import ComponentInfo from '../generic/cd-doc-component-info.vue'
 import CDGrid from '../components/cd-grid.vue'
@@ -20,12 +22,53 @@ export default {
   },
   data (view) {
     return {
-      infos: CDGrid.mixins.concat(CDGrid),
-      crud: {
-        get: {
+      filter: [
+        {
+          datafield: 'minPopulation',
+          text: 'minPopulation'
+        },
+        {
+          datafield: 'namePrefix',
+          text: 'namePrefix'
+        },
+        {
+          datafield: 'wikiDataId',
+          text: 'Страна',
+          input: 'select',
+          valuekey: 'wikiDataId',
+          labelkey: 'name',
+          url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/countries',
           method: 'get',
-          url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities'
+          resolveresult: (response) => response.data.data,
+          resolvepayload: (payload) => {
+            return {
+              params: {
+                limit: 10,
+                namePrefix: '',
+                offset: 0
+              }
+            }
+          },
+          isdisabled: (payload, option) => option.wikiDataId.endsWith(7),
+          onselect: (payload, option) => {
+            Vue.set(payload, 'countryIds', `${option.code},`)
+          }
         }
+      ],
+      payload2: {
+        limit: 10,
+        minPopulation: null,
+        namePrefix: null,
+        distanceUnit: null,
+        offset: 0,
+        excludedCountryIds: null
+      },
+      total2: 0,
+      cities2: [],
+      infos: CDGrid.mixins.concat(CDGrid),
+      get: {
+        method: 'get',
+        url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities'
       },
       descriptor: [
         {
@@ -90,8 +133,15 @@ export default {
       this.cities = response.data.data
       this.total = response.data.metadata.totalCount
     },
+    resolveresult2 (response) {
+      this.cities2 = response.data.data
+      this.total2 = response.data.metadata.totalCount
+    },
     onpagechange (event, pageargs) {
       this.payload.offset = pageargs.row.offset
+    },
+    onpagechange2 (event, pageargs) {
+      this.payload2.offset = pageargs.row.offset
     }
   }
 }
