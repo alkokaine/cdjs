c530ae5b22f422206d0ccd0901c2f4d20a7b82e0
sit19 <alkokaine@yandex.ru>
2021-12-26 16:50:39 +0300

    select property options

---
+++ src/examples/propertyoptions.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    datafield: 'datafield',
+    text: 'Имя свойства объекта'
+  },
+  {
+    datafield: 'text',
+    text: 'Человеческое имя свойства'
+  },
+  {
+    text: 'select',
+    descriptor: [
+      {
+        datafield: 'labelkey',
+        text: 'labelkey'
+      },
+      {
+        datafield: 'valuekey',
+        text: 'valuekey'
+      },
+      {
+        datafield: 'values',
+        text: 'values',
+        input: 'code'
+      },
+      {
+        datafield: 'url',
+        text: 'url'
+      },
+      {
+        datafield: 'method',
+        text: 'method'
+      },
+      {
+        datafield: 'resolveresult',
+        text: 'resolveresult',
+        input: 'code'
+      },
+      {
+        datafield: 'resolvepayload',
+        text: 'resolvepayload',
+        input: 'code'
+      },
+      {
+        datafield: 'isdisabled',
+        text: 'isdisabled',
+        input: 'code'
+      }
+    ]
+  }
+]
