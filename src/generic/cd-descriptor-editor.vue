b90732691851e49bb182f651a5db8a40c4da01b0
sit19 <alkokaine@yandex.ru>
2021-12-27 00:08:14 +0300

    cd-descriptor-editor

--- src/generic/cd-descriptor-editor.vue
+++ src/generic/cd-descriptor-editor.vue
@@ -1,13 +1,16 @@
 <template>
-  <cd-list class="cd-descriptor-editor" :collection="descriptor" keyfield="datafield"  :listitemclicked="selectproperty">
-    <cd-form slot="header" :descriptor="propertydescriptor" :payload="selected"></cd-form>
-    <code slot-scope="scope">{{ scope.row }}</code>
+  <cd-list class="cd-descriptor-editor" :collection="descriptor" keyfield="datafield"  :listitemclicked="selectproperty" listclass="property-list" rowclass="cd-code--example">
+    <cd-form slot="header" :descriptor="propertydescriptor" :payload="selected" :onpropertychange="updateproperty" class="property-editor"></cd-form>
+    <div class="card" slot-scope="scope">
+      <code class="card-body">{{ scope.row }}</code>
+    </div>
   </cd-list>
 </template>
 
 <script>
 import options from '../examples/propertyoptions'
 import CDList from '../components/cd-list.vue'
+import CDForm from '../components/cd-form.vue'
 
 export default {
   name: 'cd-descriptor-editor',
@@ -15,7 +18,8 @@ export default {
     descriptor: { type: Array, required: true }
   },
   components: {
-    'cd-list': CDList
+    'cd-list': CDList,
+    'cd-form': CDForm
   },
   data (editor) {
     return {
@@ -25,12 +29,33 @@ export default {
   },
   methods: {
     selectproperty (event, args) {
-      console.log(event, args)
+      this.selected = args.row
+    },
+    updateproperty (property, value) {
+      this.selected[property.datafield] = value
     }
   }
 }
 </script>
 
 <style>
-
+  .cd-code--example {
+    white-space: pre-wrap;
+    width: 250px;
+    display: block;
+    margin: 10px;
+  }
+  .property-editor {
+    width: 30%;
+  }
+  .property-list {
+    width: 70%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .cd-descriptor-editor {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+  }
 </style>
