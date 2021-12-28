1cafb56076bddce069c8a64dcb83f28734890e69
sit19 <alkokaine@yandex.ru>
2021-12-28 23:54:19 +0300

    prepare to carousel

---
+++ src/components/cd-carousel.vue
@@ -0,0 +1,19 @@
+<template>
+  <cd-list :collection="carousel"></cd-list>
+</template>
+
+<script>
+import CDList from './cd-list.vue'
+export default {
+    components: {
+        'cd-list': CDList
+    },
+    props: {
+        carousel: { type: Array }
+    }
+}
+</script>
+
+<style>
+
+</style>
