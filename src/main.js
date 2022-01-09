ce82f673e26764dcc1553f0963387d50ee23044f
sit19 <alkokaine@yandex.ru>
2022-01-10 01:06:26 +0300

    + element-ui

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import 'bootstrap'
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import ruLocale from 'element-ui/lib/locale/lang/ru-RU'
 import router from './router/index'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI, { locale: ruLocale })
 new Vue({
   router: router,
   render: h => h(App)
