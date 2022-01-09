f43e08ce05fa1c6c22e64b54367214cdaa5e5c26
sit19 <alkokaine@yandex.ru>
2022-01-10 01:08:03 +0300

    props list

--- src/generic/cd-prop-list.vue
+++ src/generic/cd-prop-list.vue
@@ -1,36 +1,73 @@
 <template>
-  <cd-list class="cd-properties" listclass="property-list" :collection="descriptor" keyfield="datafield" :listitemclicked="onpropertyclick">
-    <div class="property-descriptor" slot-scope="property">
-      <button type="button" class="btn btn-link btn-sm bi bi-x-circle" v-on:click.stop="removeproperty($event, property.index)" data-bs-toggle="tooltip" data-bs-placement="top" title="Удалить"></button>
-      <button type="button" class="property-descriptor btn btn-link btn-sm">{{ property.row.datafield }}</button>
+  <cd-list class="cd-properties" listclass="property-list" rowclass="property-descriptor" :collection="descriptor" keyfield="datafield" :listitemclicked="onpropertyclick">
+    <div class="property-descriptor--inner" slot-scope="property" draggable="true" v-on:drag="ondrag($event, property)" v-on:drop="ondrop($event, property)" v-on:dragover="ondragover($event, property)">
+      <button type="button" class="btn btn-link btn-sm bi bi-x-circle" v-on:click.stop="removeproperty($event, property.index)"></button>
+      <el-popover trigger="click" :value="(zoomprop.index === property.index)">
+        <cd-form :payload="property.row" :onpropertychange="onpropertyedited">
+          <div>
+            AAA {{ property }}
+          </div>
+        </cd-form>
+        <button slot="reference" type="button" class="property-descriptor btn btn-link btn-sm">{{ property.row.datafield }}</button>
+      </el-popover>
     </div>
     <template slot="placeholder">
-      <button type="button" class="btn btn-link btn-sm" v-on:click="generateproperties" data-bs-toggle="tooltip" data-bs-placement="top" title="Существующие дескприпторы будут удалены и заменены базовыми дескрипторами свойств первого объекта в коллекции">Сгенерировать колонки автоматически</button>
+      <button type="button" class="btn btn-link btn-sm" :class="{ 'disabled': !cangenerate }" v-on:click="generateproperties">Сгенерировать колонки автоматически</button>
       <button type="button" class="btn btn-link btn-sm" v-on:click="addproperty">Добавить колонку</button>
     </template>
   </cd-list>
 </template>
 
 <script>
+import elpopover from 'element-ui/packages/popover'
 import CDList from '../components/cd-list.vue'
+import CDForm from '../components/cd-form.vue'
+
 export default {
   name: 'cd-prop-list',
   components: {
-    'cd-list': CDList
+    'cd-list': CDList,
+    'el-popover': elpopover,
+    'cd-form': CDForm
   },
   props: {
     descriptor: { type: Array, required: true },
-    generateproperties: { type: Function, required: true }
+    generateproperties: { type: Function, required: true },
+    cangenerate: { type: Boolean, required: true }
+  },
+  data (proplist) {
+    return {
+      zoomprop: Object
+    }
+  },
+  computed: {
+    isdisabled () {
+      const list = this
+      return (scope) => scope.index !== list.index
+    }
   },
   methods: {
+    ondrag (event, property) {
+      console.log(property)
+    },
+    ondrop (event, property) {
+      console.log(property)
+    },
+    ondragover (event, property) {
+      event.preventDefault()
+      console.log(property)
+    },
     removeproperty (event, args) {
       this.descriptor.splice(args, 1)
     },
-    onpropertyclick (...args) {
-      console.log(args)
+    onpropertyclick (event, prop) {
+      this.zoomprop = prop
     },
     addproperty (...args) {
       console.log(args)
+    },
+    onpropertyedited (event, args) {
+
     }
   }
 }
@@ -42,5 +79,27 @@ export default {
   }
   .property-descriptor {
     padding-right: 5px;
+    display: inline-block;
   }
+  .property-descriptor--inner.dragElem {
+    opacity: 0.4;
+  }
+  .property-descriptor--inner {
+  }
+  [draggable] {
+    -moz-user-select: none;
+    -khtml-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+    /* Required to make elements draggable in old WebKit */
+    -khtml-user-drag: element;
+    -webkit-user-drag: element;
+  }
+  .property-drop {
+    width: 25px;
+    padding-left: 10px;
+    padding-right: 10px;
+    height: 25px;
+  }
+
 </style>
