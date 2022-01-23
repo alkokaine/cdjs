1845d9457eaa2fe5b9b6627181a0c37d7557c0d6
Алексей Коковин <alexey.kokovin@yandex.ru>
2022-01-23 19:59:17 +0300

    version

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import CDForm from './components/cd-form.vue'
 import CDGrid from './components/cd-grid.vue'
 import CDList from './components/cd-list.vue'
 import CDMenu from './components/cd-menu.vue'
+import CDMonth from './components/cd-month.vue'
 
 export default {
   CDForm,
   CDGrid,
   CDList,
-  CDMenu
+  CDMenu,
+  CDMonth
 }
