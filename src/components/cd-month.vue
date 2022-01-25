e8ff4b9ff6bd4d7d05a4e8b2c01ab7ad78cccfc1
Алексей Коковин <alexey.kokovin@yandex.ru>
2022-01-26 01:27:57 +0300

    two different modes of calendar, mode passes as number in 'payload.mode' isvisible function, indicates if day of month is visible and should be rendered, styles

--- src/components/cd-month.vue
+++ src/components/cd-month.vue
@@ -1,26 +1,33 @@
 <template>
-  <cd-list class="cd-month" listclass="cd-weekdays" :rowclass="resolverowclass" :collection="weekdays" keyfield="id">
-    <cd-list class="cd-weekday--list" slot-scope="weekday" listclass="cd-weekday--days" :class="weekday.row.class" rowclass="cd-day--container" :collection="weekdaylist(weekday)" keyfield="key">
-      <div class="cd-weekday--name" slot="header">
-        {{ weekday.row.name }}
-      </div>
-      <div class="cd-day" slot-scope="day" :class="[{ 'cd-day--prev-month': isprevmonth(day), 'cd-holiday': day.row.code === 1 }]">
-        <div class="cd-day--header">
-          <span class="cd-day--number">{{ day.row.day }}</span>
-          <span class="cd-day--info">
-            <div>{{ day.row.month_name }}</div>
-            <div>{{ day.row.weekday }}</div>
-          </span>
-        </div>
-        <div class="cd-day--content">
-          <slot :day="day.row"></slot>
-        </div>
-      </div>
-    </cd-list>
-  </cd-list>
+  <div class="calendar-container">
+    <template v-if="isweekmode">
+      <cd-list class="cd-month" listclass="cd-weekdays weeks-view row" :rowclass="resolverowclass" :collection="weekdays" keyfield="id">
+        <cd-list class="cd-weekday--list" slot-scope="weekday" listclass="cd-weekday--days" :class="weekday.row.class" rowclass="row cd-day--container" :collection="weekdaylist(weekday)" keyfield="key">
+          <div class="cd-weekday--name" slot="header">
+            {{ weekday.row.name }}
+          </div>
+          <template slot-scope="day">
+            <slot :day="day.row">
+              {{ day }}
+            </slot>
+          </template>
+        </cd-list>
+      </cd-list>
+    </template>
+    <template v-else>
+      <cd-list class="cd-month" listclass="cd-weekday--days list-view" rowclass="cd-day--container" :collection="dates" keyfield="key" :isrowvisible="isdayvisible">
+        <template slot-scope="day">
+          <slot :day="day.row">
+            {{ day }}
+          </slot>
+        </template>
+      </cd-list>
+    </template>
+  </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import CDList from './cd-list.vue'
 import moment from 'moment'
 
@@ -95,21 +102,32 @@ export default {
           hasyear = false
           console.error('[CDJS]payload object must have \'Year\' property')
         }
+        if (!Object.prototype.hasOwnProperty.call(value, 'mode')) {
+          Vue.set(value, 'mode', 0) // по умолчанию поставим 0
+        }
         if (!(hasmonth && hasyear)) return false
         const monthvalue = value.MonthID
         const ismonth = Number.isInteger(monthvalue) && (monthvalue >= 1 && monthvalue <= 12)
         if (!ismonth) console.error('[CDJS]payload.MonthID value must be Number and starting from 1, ending by 12')
         const yearvalue = value.Year
         const isyear = yearvalue !== undefined && yearvalue !== null && Number.isInteger(yearvalue)
+        const modevalue = value.mode
+        const ismode = Number.isInteger(modevalue) && [0, 1].indexOf(modevalue) !== -1
+        if (!ismode) {
+          console.warn('[CDJS]payload.mode property must be Number with value 0 or 1. It will be set to 0 (week mode) now')
+          Vue.set(value, 'mode', 0)
+        }
         return ismonth && isyear
       }
     },
-    schedule: { type: Array, default: () => ([]) },
+    schedule: { type: Array },
+    isvisible: { type: Function, default: (exist, checking) => (true) },
     showothers: { type: Boolean, default: true },
     resolvedayclass: { type: Function, default: () => ('') }
   },
   data (cdm) {
     return {
+      isweekmode: cdm.payload.mode === 0,
       weekdays: weekdays,
       keyfield: 'key',
       dates: []
@@ -125,6 +143,9 @@ export default {
         if (newvalue !== undefined && newvalue.Year !== undefined && newvalue.MonthID !== undefined) {
           calendar.$http.get(`https://isdayoff.ru/api/getdata?year=${newvalue.Year}&month=${(newvalue.MonthID)}&pre=1&covid=1&sd=0`)
             .then((response) => {
+              // приходит строчка из нулей, единиц, двоек и четвёрок, индекс символа указывает на номер дня (начиная с нуля)
+              // а значение -- код дня в производственном календаре
+              // для начала переведём строчку в массив нулей, единиц, двоек и четвёрок
               _days = Array.from(response.request.response).map(m => Number(m))
             }).catch((error) => {
               console.error(error)
@@ -143,11 +164,18 @@ export default {
                 return result.concat(days)
               }
               const dayscount = daysInMonth(newvalue.Year, newvalue.MonthID)
-              _days = (_days.length > 0
-                ? _days.map((_d, index) => ({ code: _d, d: index + 1 }))
+              _days = (_days.length > 0 // на предыдущем шаге удалось получить проиводственный календарь за этот месяц?
+                ? _days.map((_d, index) => ({ code: _d, d: index + 1 })) // отмаппим массив нулей, единиц, двоек и четвёрок в массив обьектов
+                // { code: Number (код календарного дня), d: index + 1 (день месяца, человеческий) }
+                // если же не получилось с произовдственным календарём
                 : Array.from(Array(dayscount).keys()).map((_d, index) => ({ d: index + 1 })))
                 .map(d => (day(date(newvalue.Year, newvalue.MonthID, d.d).toDate(), d.code)))
-              calendar.dates = prepend(_days)
+                // и ещё раз отмаппим, через функцию day, которая вернёт всё, что нам нужно знать об этом дне
+              if (newvalue.mode === 0) {
+                calendar.dates = prepend(_days)
+              } else {
+                calendar.dates = _days
+              }
             })
         }
       }
@@ -161,72 +189,69 @@ export default {
     isprevmonth () {
       const calendar = this
       return (weekday) => weekday.row.month !== (calendar.payload.MonthID - 1)
+    },
+    isdayvisible () {
+      const calendar = this
+      return (weekday) => {
+        if (calendar.schedule === undefined) return true
+        if (typeof calendar.schedule === 'function') return true
+        return calendar.schedule.findIndex(exists => calendar.isvisible(exists, weekday)) >= 0
+      }
     }
   },
   methods: {
     resolverowclass (row) {
       const cdmonth = this
-      return `cd-weekday-container ${([0, 6].indexOf(row.weekdayNumber) !== -1 ? 'cd-day--weekend' : '')} ${cdmonth.resolvedayclass(row)}`
+      return `cd-weekday-container col ${([0, 6].indexOf(row.weekdayNumber) !== -1 ? 'cd-day--weekend' : '')} ${cdmonth.resolvedayclass(row)}`
     }
   }
 }
 </script>
 
 <style>
-  .cd-day--container {
-      padding: 5px;
-      border: 1px solid;
-      border-radius: 0.5em;
-      border-color: rgba(0, 0, 0, 0.205);
-      margin-bottom: 5px;
-      min-height: 125px;
-  }
-  .cd-days--list {
+  .cd-weekday--days {
+    padding-inline-start: unset;
     list-style: none;
-    margin: 0 0 0 0;
-    padding: 0;
-  }
-  .cd-day--info {
-    line-height: 1;
-  }
-  .cd-days--list > li {
-    width: 14%;
-  }
-  .cd-day--header {
-    display: flex;
-    text-align: right;
-    justify-content: space-around;
-    align-items: center;
-  }
-  .cd-day--number {
-    font-size: 2em;
-    font-weight: bold;
   }
   .cd-weekdays {
-    list-style: none;
-    width: 100%;
     padding-inline-start: unset;
-    display: inline-flex;
-    justify-content: space-between;
-  }
-  .cd-weekday-container {
-    width: 14%;
-  }
-  .cd-weekend {
-    color: salmon;
+    list-style: none;
+    margin-right: unset;
+    margin-left: unset;
   }
   .cd-holiday {
     color: salmon;
   }
-  .cd-weekday--days {
-    list-style: none;
-    padding-inline-start: unset;
+  .cd-weekend {
+    color: salmon;
+  }
+  .cd-day--header {
+    justify-content: space-between;
+    padding-left: 1em;
+    padding-right: 1em;
+  }
+  .cd-day--number {
+    font-size: 2em;
+    font-weight: bolder;
+  }
+  .cd-day--container {
+    margin-left: 0.5em;
+    margin-right: 0.5em;
+    flex-grow: 1;
   }
   .cd-day--content {
     border-top: 1px solid;
-    color: black;
   }
-  .cd-day--prev-month {
-    opacity: 0.5;
+  .cd-day--info {
+    text-align: right;
+  }
+  .cd-day {
+    border: 1px solid;
+    border-color: #00000023;
+    border-radius: 0.5em;
+  }
+  .col {
+    padding-left: unset;
+    padding-right: unset;
   }
 </style>
