d8fc0f03620f255ab42c2c138708ab6cfc3e5929
Алексей Коковин <alexey.kokovin@yandex.ru>
2022-01-26 01:18:45 +0300

    day component

---
+++ src/components/cd-day.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="cd-day" :class="{ 'cd-holiday': day.code == 1 }">
+    <div class="cd-day--header row">
+      <div class="cd-day--number col">{{ day.day }}</div>
+      <div class="cd-day--info col">
+        <slot name="info"></slot>
+      </div>
+    </div>
+    <div class="cd-day--content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cd-day',
+  props: {
+    day: { type: Object, required: true }
+  }
+}
+</script>
+
+<style>
+  .cd-day--content {
+    height: 125px;
+  }
+</style>
