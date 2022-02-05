2839a9bb4345606e4fe36ceff32d5b53cf763ce1
Алексей Коковин <alexey.kokovin@yandex.ru>
2022-02-05 17:26:40 +0300

    tab-name property

--- src/components/cd-tabs.vue
+++ src/components/cd-tabs.vue
@@ -1,6 +1,6 @@
 <template>
   <cd-list :collection="tabs" keyfield="tabkey" listclass="nav nav-tabs mp3" :listrole="keyfield" :listitemclicked="ontabselected">
-    <button slot-scope="scope" :ref="tabid(scope.index)" class="nav-link" role="tab" :class="{ 'active': scope.index === current }" :tabindex="scope.index">{{ scope.row.caption }}</button>
+    <button slot-scope="scope" :ref="tabid(scope.index)" class="nav-link" role="tab" :class="{ 'active': scope.index === current }" :tabindex="scope.index">{{ scope.row[name] }}</button>
     <div class="tab-pane" slot="footer">
       <slot></slot>
     </div>
@@ -18,7 +18,8 @@ export default {
     tabs: { type: Array, required: true, description: 'список вкладок' },
     ontabselected: { type: Function, required: true, description: 'Что произойдёт при выборе вкладки' },
     keyfield: { type: String, default: 'tabkey', description: 'Свойство-идентификатор вкладки' },
-    current: { type: Number, default: 0, description: 'Выбранный индекс' }
+    current: { type: Number, default: 0, description: 'Выбранный индекс' },
+    name: { type: String, default: 'caption', description: 'Свойство, где находится заголовок вкладки' }
   },
   data (tabcontrol) {
     return {
