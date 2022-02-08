04b1c1686f5df69037a5abc1b9eb2ce5f0aad384
Алексей Коковин <alexey.kokovin@yandex.ru>
2022-02-08 22:52:44 +0300

    styles to month, cleaning

--- src/components/cd-day.vue
+++ src/components/cd-day.vue
@@ -14,8 +14,7 @@
 </template>
 
 <script>
-import month from '@/common/calendar-mixin'
-import moment from 'moment'
+import month from '../common/calendar-mixin'
 const weekdayFormatter = new Intl.DateTimeFormat('ru-RU', { weekday: 'short' })
 const monthFormatter = new Intl.DateTimeFormat('ru-RU', { month: 'short' })
 const dayFormatter = new Intl.DateTimeFormat('ru-RU', { day: '2-digit' })
@@ -31,56 +30,15 @@ export default {
     }
   },
   computed: {
-    date () {
-      const day = this
-      const resolveyear = day.info.isprev && day.info.month === 12
-        ? day.payload.Year - 1
-        : day.payload.Year
-      return moment([resolveyear, day.info.month - 1, day.info.day]).toDate()
-    },
     day () {
-      return dayFormatter.format(this.date)
+      return dayFormatter.format(this.info.date)
     },
     weekday () {
-      return weekdayFormatter.format(this.date)
+      return weekdayFormatter.format(this.info.date)
     },
     month () {
-      return monthFormatter.format(this.date)
+      return monthFormatter.format(this.info.date)
     }
   }
 }
 </script>
-
-<style>
-  .compact {
-    margin-bottom: unset!important;
-  }
-  .cd-day--number {
-    font-size: 2em;
-    font-weight: bold;
-    padding-left: 1em;
-  }
-  .cd-day {
-    border: 1px solid;
-    margin-bottom: 1em;
-  }
-  .cd-day:hover {
-    box-shadow: 0 0.25em 1em rgb(99, 99, 99);
-  }
-  .cd-day--info {
-    width: 20%;
-    line-height: 1em;
-    margin: auto;
-    text-align: right;
-    padding-right: 2em;
-}
-  .cd-day--header {
-    cursor: default;
-    user-select: none;
-  }
-  .cd-day--content {
-    padding: 0.5em;
-    border-top: 1px solid;
-    color: unset;
-  }
-</style>
