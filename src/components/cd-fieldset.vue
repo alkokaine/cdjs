163be4bd48592be7e021204ed5d106b117e8308e
Алексей Коковин <KokovinAA@cross-d.ru>
2022-04-27 12:28:09 +0300

    slots

--- src/components/cd-fieldset.vue
+++ src/components/cd-fieldset.vue
@@ -1,44 +1,20 @@
 <template>
   <fieldset class="cd-fieldset d-block">
     <slot></slot>
-    <!-- <div class="cd-field" :class="[{ 'inner' : inner}, property.class]" v-for="(property,index) in visibleproperties" :key="propertykey(property,index)">
-      <template v-if="hasdescriptor(property)">
-        <cd-fieldset class="inner"
-            :editmode="editmode"
-            :class="property.class"
-            :isvisible="isvisible"
-            :propertyconfig="propertyconfig"
-            :descriptor="property.descriptor"
-            :readonly="readonly"
-            :resolvefieldclass="resolvefieldclass"
-            :inner="true"
-            :inline="inline">
-            <legend class="cd-legend w-auto form-label">{{ property.text }}</legend>
-        </cd-fieldset>
-      </template>
-      <template v-else>
-        <cd-cell :class="[resolvefieldclass(property), { 'inner': inner }]" :config="propertyconfig(property)" :readonly="!editmode || readonly(property)">
-          <label class="cd-label form-label mb-0" :for="property.datafield" slot="label">{{ property.text }}</label>
-        </cd-cell>
-      </template>
-    </div> -->
     <cd-list listclass="list-unstyled cd-fieldset--inner" keyfield="datafield" :rowclass="['cd-field', { 'inner': inner }]" :isrowvisible="isvisible" :collection="descriptor">
       <div class="d-block cd-fieldset--header w-auto" slot="header"><slot name="legend"></slot></div>
-      <template slot-scope="property">
-        <cd-fieldset v-if="property.row.descriptor" :descriptor="property.row.descriptor" :isvisible="isvisible" :resolvevalue="resolvevalue" :isdisabled="isdisabled" :parent="property.row"
-          :onchange="onchange" :onclear="onclear" :onfocus="onfocus" :oninput="oninput" :onblur="onblur" :onselect="onselect">
-          <legend v-if="haslegend(property.row)" slot="legend">{{ property.row.text }}</legend>
-        </cd-fieldset>
-        <slot v-else>
-          <cd-cell class="cd-field" :value="resolvevalue(property.row)" :property="property" :disabled="isdisabled(property.row)"
-            :class="property.row.class" :onchange="onchange" :onblur="onblur" :onclear="onclear" :oninput="oninput" :onfocus="onfocus"
-            :onselect="onselect">
-            <el-popover slot="label" :disabled="property.isvalid">
-              <span>A</span>
-              <label slot="reference" class="cd-label form-label mb-0" :for="property.row.datafield">{{ property.row.text }}</label>
-            </el-popover>
-          </cd-cell>
-        </slot>
+      <template slot-scope="{ row }">
+        <template v-if="row.descriptor">
+          <cd-fieldset :descriptor="row.descriptor" :isvisible="isvisible" :isdisabled="isdisabled" :parent="row" :resolvevalue="resolvevalue">
+            <legend v-if="haslegend(row)" slot="legend">{{ row.text }}</legend>
+            <template slot-scope="{ property }">
+              <slot :property="property" :parent="row"/>
+            </template>
+          </cd-fieldset>
+        </template>
+        <template v-else>
+          <slot :property="row" :parent="parent"></slot>
+        </template>
       </template>
     </cd-list>
   </fieldset>
@@ -46,7 +22,6 @@
 
 <script>
 import props from '../common/property-decorator'
-import cell from './cd-cell.vue'
 import list from './cd-list.vue'
 export default {
   name: 'cd-fieldset',
@@ -56,15 +31,9 @@ export default {
     inner: { type: Boolean, default: false },
     resolvevalue: { type: Function, required: true },
     isdisabled: { type: Function, default: (property) => (false) },
-    onchange: { type: Function },
-    onblur: { type: Function },
-    onfocus: { type: Function },
-    oninput: { type: Function },
-    onclear: { type: Function },
-    onselect: { type: Function }
+    isvalid: { type: Function, default: (property) => (false) }
   },
   components: {
-    'cd-cell': cell,
     'cd-list': list
   },
   computed: {
