eb4038d58fd32269a80faef65607b9631d0359fc
Алексей Коковин <KokovinAA@cross-d.ru>
2022-04-28 11:17:52 +0300

    examples

--- src/views/cd-doc-form.vue
+++ src/views/cd-doc-form.vue
@@ -25,324 +25,334 @@ export default {
       },
       descriptor: [
         {
-          text: 'Общее',
           descriptor: [
             {
-              datafield: 'Name',
-              text: 'Полное название'
-            },
-            {
+              text: 'Общее',
               descriptor: [
                 {
-                  datafield: 'ShortName',
-                  text: 'Сокращенное название'
-                }, {
-                  datafield: 'UltraShortName',
-                  text: 'Краткое название'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'Is24Group',
-                  text: 'Is24Group'
+                  datafield: 'Name',
+                  text: 'Полное название'
                 },
                 {
-                  datafield: 'IsBudget',
-                  text: 'IsBudget'
-                  // Invalid prop: type check failed for prop "value".
+                  descriptor: [
+                    {
+                      datafield: 'ShortName',
+                      text: 'Сокращенное название'
+                    }, {
+                      datafield: 'UltraShortName',
+                      text: 'Краткое название'
+                    }
+                  ],
+                  class: 'row row-cols-2'
                 },
                 {
-                  datafield: 'IsDepartment',
-                  text: 'IsDepartment'
-                }
-              ],
-              class: 'row row-cols-3'
-            },
+                  descriptor: [
+                    {
+                      datafield: 'Is24Group',
+                      text: 'Is24Group'
+                    },
+                    {
+                      datafield: 'IsBudget',
+                      text: 'IsBudget'
+                      // Invalid prop: type check failed for prop "value".
+                    },
+                    {
+                      datafield: 'IsDepartment',
+                      text: 'IsDepartment'
+                    }
+                  ],
+                  class: 'row row-cols-3'
+                },
 
-            {
-              descriptor: [
                 {
-                  datafield: 'IsFew',
-                  text: 'Малочисленная'
+                  descriptor: [
+                    {
+                      datafield: 'IsFew',
+                      text: 'Малочисленная'
+                    },
+                    {
+                      datafield: 'WorkDaysCount',
+                      text: 'WorkDaysCount'
+                    },
+                    {
+                      datafield: 'ObjectID',
+                      text: 'ObjectID'
+                    },
+                    {
+                      datafield: 'slider',
+                      text: 'slider',
+                      input: 'slider'
+                    }
+                  ],
+                  class: 'row row-cols-4'
                 },
-                {
-                  datafield: 'WorkDaysCount',
-                  text: 'WorkDaysCount'
-                },
-                {
-                  datafield: 'ObjectID',
-                  text: 'ObjectID'
-                },
-                {
-                  datafield: 'slider',
-                  text: 'slider',
-                  input: 'slider'
-                }
-              ],
-              class: 'row row-cols-4'
-            },
 
-            {
-              descriptor: [
                 {
-                  datafield: 'ObjectStatusID',
-                  text: 'ObjectStatusID'
+                  descriptor: [
+                    {
+                      datafield: 'ObjectStatusID',
+                      text: 'ObjectStatusID'
+                    },
+                    {
+                      datafield: 'ObjectTypeID',
+                      text: 'ObjectTypeID'
+                    },
+                    {
+                      datafield: 'MainObjectID',
+                      text: 'MainObjectID'
+                    }
+                  ],
+                  class: 'row row-cols-3'
                 },
-                {
-                  datafield: 'ObjectTypeID',
-                  text: 'ObjectTypeID'
-                },
-                {
-                  datafield: 'MainObjectID',
-                  text: 'MainObjectID'
-                }
-              ],
-              class: 'row row-cols-3'
-            },
 
-            {
-              descriptor: [
                 {
-                  datafield: 'Code',
-                  text: 'Code'
+                  descriptor: [
+                    {
+                      datafield: 'Code',
+                      text: 'Code'
+                    },
+                    {
+                      datafield: 'EndDate',
+                      text: 'EndDate'
+                    },
+                    {
+                      datafield: 'OrderNumber',
+                      text: 'OrderNumber'
+                    }
+                  ],
+                  class: 'row row-cols-3'
                 },
                 {
-                  datafield: 'EndDate',
-                  text: 'EndDate'
-                },
-                {
-                  datafield: 'OrderNumber',
-                  text: 'OrderNumber'
+                  datafield: 'ParentObjectID',
+                  text: 'ParentObjectID'
                 }
-              ],
-              class: 'row row-cols-3'
+              ]
             },
             {
-              datafield: 'ParentObjectID',
-              text: 'ParentObjectID'
+              text: 'Адрес и контактные данные',
+              descriptor: [
+                {
+                  descriptor: [
+                    {
+                      datafield: 'PostIndex',
+                      text: 'Почтовый индекс'
+                    },
+                    {
+                      datafield: 'City',
+                      text: 'Город'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'Region',
+                      text: 'Район'
+                    },
+                    {
+                      datafield: 'Street',
+                      text: 'Улица'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'HouseNumber',
+                      text: 'Дом'
+                    },
+                    {
+                      datafield: 'HouseBlock',
+                      text: 'Корпус'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'PhoneNumber',
+                      text: 'Телефон'
+                    },
+                    {
+                      datafield: 'EMail',
+                      text: 'E-mail'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'FaxNumber',
+                      text: 'Факс'
+                    },
+
+                    {
+                      datafield: 'WWW',
+                      text: 'Сайт'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  datafield: 'CityID',
+                  text: 'CityID'
+                }
+              ]
             }
-          ]
+          ],
+          class: 'row row-cols-2'
         },
         {
-          text: 'Адрес и контактные данные',
           descriptor: [
             {
+              text: 'Реквизиты',
               descriptor: [
                 {
-                  datafield: 'PostIndex',
-                  text: 'Почтовый индекс'
+                  descriptor: [{
+                    datafield: 'INN',
+                    text: 'ИНН'
+                  },
+                  {
+                    datafield: 'OKPO',
+                    text: 'ОКПО'
+                  }
+                  ],
+                  class: 'row row-cols-2'
                 },
                 {
-                  datafield: 'City',
-                  text: 'Город'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'Region',
-                  text: 'Район'
-                },
-                {
-                  datafield: 'Street',
-                  text: 'Улица'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'HouseNumber',
-                  text: 'Дом'
-                },
-                {
-                  datafield: 'HouseBlock',
-                  text: 'Корпус'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'PhoneNumber',
-                  text: 'Телефон'
-                },
-                {
-                  datafield: 'EMail',
-                  text: 'E-mail'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'FaxNumber',
-                  text: 'Факс'
-                },
+                  descriptor: [
 
+                    {
+                      datafield: 'KPP',
+                      text: 'КПП'
+                    },
+                    {
+                      datafield: 'OKVED',
+                      text: 'ОКВЭД'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
                 {
-                  datafield: 'WWW',
-                  text: 'Сайт'
+                  descriptor: [
+                    {
+                      datafield: 'BankName',
+                      text: 'Банк'
+                    },
+                    {
+                      datafield: 'CorrAccount',
+                      text: 'Корр/счет'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'BIC',
+                      text: 'БИК'
+                    },
+                    {
+                      datafield: 'CorrBank',
+                      text: 'Корр.банк'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  datafield: 'CurrentAccount',
+                  text: 'Р/Счет'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'PersonalAccount',
+                      text: 'Лицевой счет'
+                    },
+                    {
+                      datafield: 'PersonalAccount2',
+                      text: 'Лицевой счет2'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  datafield: 'MunAccount',
+                  text: 'MunAccount'
+                },
+                {
+                  datafield: 'UFK',
+                  text: 'UFK'
                 }
-              ],
-              class: 'row row-cols-2'
+              ]
             },
             {
-              datafield: 'CityID',
-              text: 'CityID'
+              text: 'Руководитель',
+              descriptor: [
+                {
+                  datafield: 'HeadPosition',
+                  text: 'Должность'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'HeadFIO',
+                      text: 'ФИО'
+                    },
+                    {
+                      datafield: 'IsIO',
+                      text: 'Исполняющий обязаности'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'HeadPhoneNumber',
+                      text: 'Телефон'
+                    },
+                    {
+                      datafield: 'HeadEMail',
+                      text: 'E-mail'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'HeadReason',
+                      text: 'HeadReason'
+                    },
+                    {
+                      datafield: 'HeadSignature',
+                      text: 'Подпись для документов'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                },
+                {
+                  descriptor: [
+                    {
+                      datafield: 'PostAddress',
+                      text: 'PostAddress'
+                    },
+                    {
+                      datafield: 'LawAddress',
+                      text: 'LawAddress'
+                    }
+                  ],
+                  class: 'row row-cols-2'
+                }
+              ]
             }
-          ]
-        },
-        {
-          text: 'Реквизиты',
-          descriptor: [
-            {
-              descriptor: [{
-                datafield: 'INN',
-                text: 'ИНН'
-              },
-              {
-                datafield: 'OKPO',
-                text: 'ОКПО'
-              }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-
-                {
-                  datafield: 'KPP',
-                  text: 'КПП'
-                },
-                {
-                  datafield: 'OKVED',
-                  text: 'ОКВЭД'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'BankName',
-                  text: 'Банк'
-                },
-                {
-                  datafield: 'CorrAccount',
-                  text: 'Корр/счет'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'BIC',
-                  text: 'БИК'
-                },
-                {
-                  datafield: 'CorrBank',
-                  text: 'Корр.банк'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              datafield: 'CurrentAccount',
-              text: 'Р/Счет'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'PersonalAccount',
-                  text: 'Лицевой счет'
-                },
-                {
-                  datafield: 'PersonalAccount2',
-                  text: 'Лицевой счет2'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              datafield: 'MunAccount',
-              text: 'MunAccount'
-            },
-            {
-              datafield: 'UFK',
-              text: 'UFK'
-            }
-          ]
-        },
-        {
-          text: 'Руководитель',
-          descriptor: [
-            {
-              datafield: 'HeadPosition',
-              text: 'Должность'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'HeadFIO',
-                  text: 'ФИО'
-                },
-                {
-                  datafield: 'IsIO',
-                  text: 'Исполняющий обязаности'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'HeadPhoneNumber',
-                  text: 'Телефон'
-                },
-                {
-                  datafield: 'HeadEMail',
-                  text: 'E-mail'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'HeadReason',
-                  text: 'HeadReason'
-                },
-                {
-                  datafield: 'HeadSignature',
-                  text: 'Подпись для документов'
-                }
-              ],
-              class: 'row row-cols-2'
-            },
-            {
-              descriptor: [
-                {
-                  datafield: 'PostAddress',
-                  text: 'PostAddress'
-                },
-                {
-                  datafield: 'LawAddress',
-                  text: 'LawAddress'
-                }
-              ],
-              class: 'row row-cols-2'
-            }
-          ]
+          ],
+          class: 'row row-cols-2'
         }
       ],
       content: [
