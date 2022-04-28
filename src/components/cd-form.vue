f7e3c1f44d563b43a1e8eaadae256a148e58d33f
Алексей Коковин <KokovinAA@cross-d.ru>
2022-04-28 11:12:35 +0300

    locking popover

--- src/components/cd-form.vue
+++ src/components/cd-form.vue
@@ -13,7 +13,7 @@
                 <cd-cell v-if="property" class="cd-field" :property="property"
                   :class="property.class" :onchange="onchange" :onblur="onblur" :onclear="onclear" :oninput="oninput" :onfocus="onfocus"
                   :onselect="onselect" :disabled="!editmode" :value="formobject[property.datafield]" :revert="revert">
-                  <el-popover slot="label" :disabled="property.isvalid">
+                  <el-popover slot="label" :disabled="true">
                     <label slot="reference" class="cd-label form-label mb-0" :for="property.datafield">{{ property.text }}</label>
                   </el-popover>
                 </cd-cell>
