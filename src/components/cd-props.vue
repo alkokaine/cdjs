ce890f7dee6b94a10a1f7504d0215ffdf839b2b5
Алексей Коковин <KokovinAA@cross-d.ru>
2022-05-04 09:13:56 +0300

    ishtml, isemail, iswww formatting

--- src/components/cd-props.vue
+++ src/components/cd-props.vue
@@ -1,11 +1,24 @@
 <template>
-  <cd-list :collection="descriptor" keyfield="datafield" listclass="list-unstyled m-0 text-start" :showitems="hasdata">
+  <cd-list class="cd-props" :collection="descriptor" keyfield="datafield" :listclass="['list-unstyled m-0 text-start', parentclass]" :showitems="hasdata">
     <span v-if="!hasdata" slot="header">
       н/д
     </span>
-    <template slot-scope="prop">
-      <cd-props v-if="prop.row.descriptor" :descriptor="prop.row.descriptor" :payload="payload" :class="prop.row.class"></cd-props>
-      <div v-else-if="prop.row.datafield" class="cd-prop" :class="prop.row.class">{{ payload[prop.row.datafield] }}</div>
+    <template slot-scope="{ row }">
+      <cd-props v-if="row.descriptor" :descriptor="row.descriptor" :payload="payload" :parentclass="row.class"></cd-props>
+      <div v-else-if="row.datafield" class="cd-prop" :class="row.class">
+        <template v-if="ishtml(payload[row.datafield])">
+          <span v-html="payload[row.datafield]"></span>
+        </template>
+        <template v-else-if="isemail(row)">
+          <a :href="email(payload[row.datafield])">{{ payload[row.datafield] }}</a>
+        </template>
+        <template v-else-if="iswww(row)">
+          <a :href="payload[row.datafield]">{{ payload[row.datafield] }}</a>
+        </template>
+        <template v-else>
+          {{ payload[row.datafield] }}
+        </template>
+      </div>
     </template>
   </cd-list>
 </template>
@@ -16,7 +29,8 @@ export default {
   name: 'cd-props',
   props: {
     descriptor: { type: Array, required: true },
-    payload: { type: Object }
+    payload: { type: [Object, Error] },
+    parentclass: { type: String, default: 'd-block' }
   },
   components: {
     'cd-list': CDList
@@ -24,6 +38,23 @@ export default {
   data (props) {
     return {
       hasdata: props.payload !== null && props.payload !== undefined
+    }
+  },
+  computed: {
+    iswww () {
+      return (property) => property.format === 'www'
+    },
+    isemail () {
+      return (property) => property.format === 'email'
+    },
+    email () {
+      return (value) => `mailto:${value}`
+    }
+  },
+  methods: {
+    ishtml (string) {
+      var doc = new DOMParser().parseFromString(string, 'text/html')
+      return Array.from(doc.body.childNodes).some(node => node.nodeType === 1)
     }
   }
 }
