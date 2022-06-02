186855dbf3f04bb394c39f8169dac6829aaff847
Алексей Коковин <KokovinAA@cross-d.ru>
2022-06-02 11:56:59 +0300

    loaddata

--- src/common/collection.js
+++ src/common/collection.js
@@ -21,9 +21,6 @@ export default {
       returns: Array,
       description: 'возвращает ту часть ответа сервера, которую мы собирались поместить в коллекцию'
     },
-    onerror: {
-      type: Function
-    },
     /**
      * собственно, коллекция
      */
@@ -32,12 +29,9 @@ export default {
       default: () => ([]),
       description: 'непосредственно коллекция, массив объектов'
     },
-    /**
-    * payload: объект, по которому клиенту возвращается набор записей
-    */
     payload: {
       type: Object,
-      description: 'нужно смириться с тем, что почти всегда сервер от нас что-то ожидает, и это что-то должно передаваться в это свойство'
+      description: 'на случай, если что-то надо передать вместе с запросом'
     },
     /**
       * keyfield: имя свойства с ключевым значением
@@ -50,12 +44,9 @@ export default {
      * как правило, ссылки получались постоянные, но чем чёрт не шутит,
      * пусть это будет функцией от параметров загрузки данных, см. watch.payload
      */
-    // crud: { type: [Object, Function], description: '!!!!! кандидат на пересмотр !!!!! Пусть это будет объект с четырьма свойствами: get, update, delete, add { method: String, url: String }, для получения коллекции, добавления, удаления и редактирования объектов коллекци. В свойстве method будем указывать метод http запроса (post, get...) в свойстве url адрес метода ' },
     get: { type: Object, description: 'Объект, содержащий урл, наименование хттп-метода, и возможно заголовки запроса' },
-    /**
-     * нужна ли постраничная загрузка
-     */
-    paging: { type: Boolean, default: false, description: 'Нужна ли постраничная загрузка' }
+    beforeRequest: { type: Function, description: 'Функция выполнится перед отправкой запроса' },
+    errorRequest: { type: Function, description: 'Функция выполнится при ошибке' }
   },
   computed: {
     rowkey () {
@@ -65,21 +56,41 @@ export default {
   },
   data (col) {
     return {
-      error: false
+      error: false,
+      isloading: false
     }
   },
   methods: {
     loaddata (url, payload) {
       const local = this
-      local.$http({ method: local.get.method, url: url, data: local.resolvepayload(payload), headers: local.get.headers })
-        .then((response) => {
-          local.resolveresult(response)
-          local.error = false
-        })
-        .catch((reason) => {
-          local.error = reason
-          if (local.onerror !== undefined && typeof local.onerror === 'function') local.onerror(reason)
-        })
+      const onbefore = function (config) {
+        if (local.beforeRequest !== undefined) return local.beforeRequest(config)
+        local.isloading = true
+        return config
+      }
+      const onerror = function (error) {
+        if (local.errorRequest !== undefined) local.errorRequest(() => Promise.reject(error))
+        setTimeout(() => { local.isloading = false }, 100)
+        return error
+      }
+      const onafter = function (response) {
+        setTimeout(() => { local.isloading = false }, 100)
+        return response
+      }
+      const axios = local.$http.create()
+      axios.interceptors.request.use(onbefore, onerror)
+      axios.interceptors.response.use(onafter, onerror)
+      axios({
+        method: local.get.method,
+        url: url,
+        data: local.resolvepayload(payload),
+        headers: local.get.headers
+      }).then((response) => {
+        local.resolveresult(response)
+        local.error = false
+      }).catch((reason) => {
+        local.error = reason
+      })
     }
   }
 }
