1a4551cd36c820efd1980dfa91b1d2245e877575
Алексей Коковин <KokovinAA@cross-d.ru>
2023-08-03 16:08:50 +0300

    list row-class

--- src/components/cd-list.vue
+++ src/components/cd-list.vue
@@ -6,7 +6,7 @@
         <slot name="no-data"></slot>
       </li>
       <li v-for="(row, index) in filtered"
-        :key="rowkey(row)" class="cd-list--item" :class="rowclassResolved(row, index)"
+        :key="rowkey(row)" class="cd-list--item" :class="rowClassResolved(row, index)"
         v-on:click="listitemclicked($event, { row, index })"
         v-on:mouseenter="listitementered($event, { row, index })"
         role="presentation">
@@ -35,10 +35,6 @@ export default {
      * строка или функция, возвращающая css-класс для для
      */
     listclass: { type: [String, Array], description: 'Строка или массив, содержащие названия классов, которые будут применены к ul внутри cd-list' },
-    /**
-     * строка или функция, возвращающая css-класс для строки коллекции
-     */
-    rowclass: { type: [String, Array, Function], default: '', description: 'Строка или функция, возвращающая строку с классом для элемента списка, ' },
     listitemclicked: { type: Function, default: function (event, scope) {}, description: 'Функция, которая выполнится по клику на элементе списка' },
     listitementered: { type: Function, default: function (event, scope) {}, description: 'Функция, которая выполнится при входе мыши на элемент списка' },
     listitemleaved: { type: Function, default: function (event, scope) {}, description: 'Функция, которая выполнится при выходе из элемента списка' },
@@ -50,37 +46,13 @@ export default {
     inner: { type: Boolean, default: false, description: 'Признак того, что коллекция вложенная' },
     listrole: { type: String }
   },
-  watch: {
-    payload: {
-      handler (newvalue, oldvalue) {
-        if (newvalue !== undefined) this.loaddata(this.get.url, newvalue)
-      }
-    }
-  },
-  // watch: {
-  //   get: {
-  //     deep: true,
-  //     immediate: true,
-  //     handler (newvalue, oldvalue) {
-  //       if (newvalue !== undefined && newvalue !== oldvalue) this.loaddata(newvalue.url, this.payload)
-  //     }
-  //   }
-  // },
   computed: {
-    isempty () {
-      return this.filtered.length === 0
+    isempty ({ filtered }) {
+      return filtered.length === 0
     },
-    filtered () {
-      const list = this
-      if (list.isrowvisible === undefined) return list.collection
-      return list.collection.filter(row => list.isrowvisible(row))
-    },
-    rowclassResolved () {
-      const list = this
-      return (row, index) => {
-        if (typeof list.rowclass === 'string' || Array.isArray(list.rowclass)) return list.rowclass
-        return list.rowclass(row, index)
-      }
+    filtered ({ isrowvisible, collection }) {
+      if (isrowvisible === undefined) return collection
+      return collection.filter(row => isrowvisible(row))
     }
   }
 }
