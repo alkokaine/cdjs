59301d8a4c6746851073850c75f9967ba3170492
Алексей Коковин <KokovinAA@cross-d.ru>
2023-08-04 07:21:58 +0300

    реанимация календаря и экволюция вкладок

--- src/components/cd-month.vue
+++ src/components/cd-month.vue
@@ -1,419 +1,152 @@
 <template>
-  <cd-list class="cd-month container-sm" :listclass="[ 'list-unstyled', { 'row': mode }]"
-    :rowclass="rootrowclass" :collection="source" keyfield="key" :isrowvisible="isdayvisible">
-    <div class="month-header" slot="header">
-      <slot name="month-header">
-        <cd-month-header v-if="showheader" :payload="payload"></cd-month-header>
-      </slot>
-    </div>
-    <div class="month-item-wrap" slot-scope="content" :class="{ 'weekday-container': content.row.id }">
-      <template v-if="mode">
-        <cd-list class="cd-weekday--container" :collection="weekdaylist(content)" keyfield="key" listclass="list-unstyled col"
-          :rowclass="resolvedayclass" :listitemclicked="ondayselect" :isrowvisible="isdayvisible">
-          <div class="weekday-name" slot="header">
-            <div v-if="selectweekday">
-              <input type="checkbox" v-on:change="onweekdayselect($event, content)" :checked="isweekdayselected(content)"/>
-            </div>
-            {{ content.row.short_name }}
-          </div>
-          <div class="month-item-wrap" slot-scope="day"
-            :class="{ 'prev-month': day.row.isprev, 'holiday' : day.row.code == 1, 'selected': resolvedayselected(day) }">
-            <cd-day :info="day.row" :compact="compact" :tile="tile">
-              <slot :day="day.row" :index="day.index"></slot>
-            </cd-day>
-          </div>
-        </cd-list>
-      </template>
-      <template v-else>
-        <cd-day :info="content.row" :tile="tile">
-          <slot :day="content.row" :index="content.index"></slot>
-        </cd-day>
-      </template>
-    </div>
-    <template v-if="canadd">
-      <div class="cd-day new-day--template container-sm" slot="placeholder">
-        <div class="cd-day--number col btn btn-group btn-group-lg" role="group" aria-label="new-day-group">
-          <button class="btn bi bi-calendar2-week btn-link" v-on:click.stop="templateselect"></button>
-          <button class="btn bi bi-calendar3-event btn-link" v-on:click.stop="addday"></button>
+  <div class="cd-month--wrapper container">
+    <slot name="month-header"></slot>
+    <template v-if="ischedule">
+      <cd-day-grid class="cd-month" key-field="week" :compact="compact"
+        :week-range="weekRange" :days="keyedDays" :compare-date="compareDate" :select-day="onDaySelect"
+        :select-weekdays="selectWeekdays" :multiple="multiple">
+        <div slot-scope="{ day, week }">
+          <slot :day="day" :week="week"></slot>
         </div>
-      </div>
+      </cd-day-grid>
     </template>
-    <div class="month-no-data" slot="no-data">
-      Нет данных
-    </div>
-    <div v-if="!compact && createnew" slot="footer">
-      <el-dialog class="template-selector" :visible.sync="runtemplate" :close-on-click-modal="false" v-on:closed="ondialogclose">
-        <cd-form v-if="templateready" :descriptor="templatedescriptor" :payload="generatorconfig" :sync="true" :showcontrols="true" :reset="closedialog" :submit="run">
-          <cd-month ref="template" class="month-template-preview" slot="footer" :payload="generatorconfig" :canadd="false" :oncalendarready="onready"
-            :isdayselected="generatorconfig.daycompare" :ondayselect="generatorconfig.ondayselect" :onweekdayselect="weekdayselect"
-            :tile="false"
-            :isweekdayselected="resolveweekdayselected" :selectweekday="generatorconfig.template_id === 3" :compact="true" :resolvedayclass="dayclass">
-          </cd-month>
-        </cd-form>
-      </el-dialog>
-    </div>
-  </cd-list>
+    <template v-else>
+      <cd-day-tabs class="cd-month" :days="keyedDays" :select-day="onDaySelect" :orientation="orientation"
+        :compare-date="compareDate" :multiple="multiple" :selected-days="selectedDays">
+        <div slot-scope="{ day }">
+          <slot :day="day"></slot>
+        </div>
+      </cd-day-tabs>
+    </template>
+    <slot name="month-footer"></slot>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import calendar from '../common/calendar-mixin'
-import CDList from './cd-list.vue'
-import CDForm from './cd-form.vue'
-import CDDay from './cd-day.vue'
-import moment from 'moment'
-import { Dialog } from 'element-ui'
-import header from './cd-month-header.vue'
-const daysInMonth = (year, month) => (moment(`${year}-${month}`, 'YYYY-MM')).daysInMonth()
-const date = (year, month, day) => (moment(`${year}-${month}-${day}`, 'YYYY-MM-DD'))
-
+import { createDate, prevMonthDays } from '@/common/month-days'
+import utils from '@/common/utils'
+import CDDayGrid from '@/components/cd-day-grid.vue'
+import CDDayTabs from '@/components/cd-day-tabs.vue'
 export default {
   name: 'cd-month',
-  mixins: [calendar],
   components: {
-    'cd-list': CDList,
-    'cd-form': CDForm,
-    'cd-day': CDDay,
-    'el-dialog': Dialog,
-    'cd-month-header': header
+    'cd-day-grid': CDDayGrid,
+    'cd-day-tabs': CDDayTabs
   },
   props: {
-    tile: { type: Boolean, default: true },
-    compact: { type: Boolean, default: false },
-    createnew: {
-      type: Function,
-      description: 'Сколько я не думал, не нашёл лучшего варианта, кроме следующего: функция принимает массив дат, выполняет какие-то преобразования и добавляет результат в массив, переданный как свойство компонента cd-month'
-    },
-    showheader: { type: Boolean, default: true, description: 'Переключатель видимости месяца-года' },
-    canadd: { type: Boolean, default: true, description: 'Можно ли добавлять события в календарь' },
-    weekdays: {
-      type: Array,
-      default: function () {
-        return [
-          {
-            id: 1,
-            name: 'Понедельник',
-            short_name: 'ПН'
-          },
-          {
-            id: 2,
-            name: 'Вторник',
-            short_name: 'ВТ'
-          },
-          {
-            id: 3,
-            name: 'Среда',
-            short_name: 'СР'
-          },
-          {
-            id: 4,
-            name: 'Четверг',
-            short_name: 'ЧТ'
-          },
-          {
-            id: 5,
-            name: 'Пятница',
-            short_name: 'ПТ'
-          },
-          {
-            id: 6,
-            name: 'Суббота',
-            short_name: 'СБ',
-            class: 'cd-weekend'
-          },
-          {
-            id: 0,
-            name: 'Воскресенье',
-            short_name: 'ВС',
-            class: 'cd-weekend'
-          }
-        ]
+    mode: {
+      type: String,
+      validator (value) {
+        return ['list', 'schedule'].indexOf(value) !== -1
       },
-      description: 'Массив дней недели'
+      default: 'schedule',
+      description: 'Режим календаря, schedule (по умолчанию) -- месяц разбит по неделям; list -- дни идут списком'
     },
-    oncalendarready: { type: Function, description: 'Выполним, когда получен календарь на указанный в payload месяц' },
-    mode: { type: [Boolean, Number], default: 1 },
-    isdayvisible: { type: Function, default: (day) => (true) },
-    isdayselected: { type: Function },
-    format: { type: String, default: 'YYYY-MM-DD' },
-    schedule: { type: [Function, Array], description: 'Данные, которые мы хотим показать в календаре' },
-    property: { type: String, description: 'Имя свойства объекта из коллекции schedule, содержащее значение даты события' },
-    resolvedayclass: { type: [String, Array, Function], default: () => (''), description: 'Функция, возвращающая класс для элемента cd-day' },
-    ondayselect: { type: Function, default: (event, args) => {}, description: 'Функция, которая выполняется при клике на день' },
-    selectweekday: { type: Boolean, default: false },
-    onweekdayselect: { type: Function, default: (event, args) => {}, desciption: 'Функция, которая выполнится, если выбрать колонку с днём недели' },
-    isweekdayselected: { type: Function, default: (...args) => (false) }
+    compareDate: { type: Function, required: true, description: 'функция возвращает true, если даты параметров функции совпадают' },
+    sixDays: { type: Boolean, default: false, description: 'Признак шестидневной рабочей недели' },
+    prependDays: { type: Boolean, default: true, description: 'Если месяц начинается не с понедельника и prepend-days = true, то присоединим дни из предыдущего месяца так, чтобы первое число указанного месяца было в своей клетке дня недели' },
+    compact: { type: Boolean, default: false, description: 'Компактный режим' },
+    selectWeekdays: { type: Boolean, default: false, description: 'Режим выбора дня недели (появится чекбокс)' },
+    onWeekdaySelect: { type: Function, description: 'Функция, которую выполним по выбору дня недели' },
+    orientation: { type: String, description: 'Расположение ярлыков на дни месяца' },
+    multiple: { type: Boolean, default: false, description: 'Можно ли выбрать несколько дней' },
+    date: { type: [Date, String, Number] }
   },
-  data (cdm) {
+  data (calendar) {
     return {
-      generatorconfig: Object,
-      runtemplate: false,
-      templatedescriptor: [
-        {
-          datafield: 'template_id',
-          text: 'Выберите шаблон',
-          input: 'select',
-          clearable: true,
-          labelkey: 'text',
-          valuekey: 'id',
-          values: [
-            {
-              id: 1,
-              text: 'Чётные числа',
-              daycompare (dayscope) {
-                return dayscope.isprev === undefined && (dayscope.date.getDate() % 2 === 0) && (!cdm.generatorconfig.workdays || dayscope.code !== 1)
-              }
-            },
-            {
-              id: 2,
-              text: 'Нечётные числа',
-              daycompare (dayscope) {
-                return dayscope.isprev === undefined && (dayscope.date.getDate() % 2 === 1) && (!cdm.generatorconfig.workdays || dayscope.code !== 1)
-              }
-            },
-            {
-              id: 3,
-              text: 'Выбрать дни недели',
-              daycompare (dayscope) {
-                return dayscope.isprev === undefined && cdm.selectedweekdays.indexOf(dayscope.date.getDay()) !== -1 && (!cdm.generatorconfig.workdays || dayscope.code !== 1)
-              }
-            },
-            {
-              id: 4,
-              text: 'Выбрать дни вручную',
-              ondayselect (event, args) {
-                if (args.row.isprev === undefined) {
-                  const index = cdm.selecteddays.indexOf(args.row.date.getDate())
-                  if (index >= 0) cdm.selecteddays.splice(index, 1)
-                  else cdm.selecteddays.push(args.row.date.getDate())
-                }
-              },
-              daycompare (dayscope) {
-                return dayscope.isprev === undefined && cdm.selecteddays.indexOf(dayscope.date.getDate()) !== -1
-              }
-            }
-          ],
-          reset (payload, option, parent) {
-            Vue.set(payload, 'daycompare', undefined)
-            Vue.set(payload, 'ondayselect', undefined)
-          },
-          onselect (payload, option, parent) {
-            Vue.set(payload, 'daycompare', option.daycompare)
-            Vue.set(payload, 'ondayselect', option.ondayselect)
-          }
-        },
-        {
-          input: 'checkbox',
-          datafield: 'workdays',
-          text: 'Только рабочие дни',
-          disabled: true,
-          toogle (payload) {
-            cdm.generatorconfig.workdays = !cdm.generatorconfig.workdays
-          }
-        }
-      ],
-      // newtask: {},
-      selectedweekdays: [],
-      selecteddays: [],
-      calendar: [],
-      newcalendar: []
+      isLoading: false,
+      selectedWeekdays: [],
+      selectedDays: [],
+      editEvent: Object
     }
   },
-  watch: {
-    'payload.MonthID': {
-      immediate: true,
-      handler (newvalue, oldvalue) {
-        const month = this
-        if (newvalue === 13) {
-          Vue.set(month.payload, 'Year', month.payload.Year + 1)
-          Vue.set(month.payload, 'MonthID', 1)
-        } else if (newvalue === 0) {
-          Vue.set(month.payload, 'Year', month.payload.Year - 1)
-          Vue.set(month.payload, 'MonthID', 12)
+  methods: {
+    onDaySelect ($event, day, week) {
+      const findIndex = this.selectedDays.findIndex(d => d.daykey === day.daykey)
+      console.log(day, week)
+      if (findIndex === -1) {
+        if (this.multiple) {
+          this.selectedDays.push(day)
         } else {
-          let days = []
-          month.$http.get(`/dayoff/getdata?year=${month.payload.Year}&month=${newvalue}&pre=1&covid=1&sd=0`)
-            .then((response) => {
-              days = Array.from(response.request.response)
-                .map((m, index) => ({ date: date(month.payload.Year, newvalue, index + 1).toDate(), code: Number(m) }))
-            })
-            .catch((reason) => {
-              days = Array.from(Array(daysInMonth(month.payload.Year, newvalue)).keys())
-                .map((m, index) => ({ date: date(month.payload.Year, newvalue, index + 1) }))
-            })
-            .finally(() => {
-              const fd = date(month.payload.Year, newvalue, days[0].date.getDate())
-              if (month.oncalendarready !== undefined) {
-                month.oncalendarready(days)
-              }
-              if (fd.day() === 1) month.calendar = days
-              const result = []
-              let ln = fd.day() - 1
-              if (ln < 0) ln = 6 - ln - 1
-              while (ln > 0) {
-                const d = fd.subtract(1, 'days')
-                // date(month.payload.Year, month.payload.MonthID)
-                result.unshift({ date: d.toDate(), isprev: true })
-                ln -= 1
-              }
-              month.calendar = result.concat(days)
-            })
+          this.selectedDays = [day]
         }
-      }
-    },
-    runtemplate: {
-      handler (newvalue) {
-        if (newvalue === true) {
-          this.generatorconfig = {
-            template_id: null,
-            workdays: false,
-            MonthID: this.payload.MonthID,
-            Year: this.payload.Year
-          }
-        } else {
-          this.generatorconfig = Object
-        }
+      } else {
+        this.selectedDays.splice(findIndex, 1)
       }
     }
   },
   computed: {
-    dayclass () {
-      return this.payload.template_id === 4 ? 'selectable' : ''
+    calendarDate ({ date }) {
+      const _date = new Date(date)
+      return createDate(_date.getFullYear(), _date.getMonth() + 1, _date.getDate())
     },
-    islistview () {
-      return !this.mode
+    ischedule ({ mode }) {
+      return mode === 'schedule'
     },
-    daynumber () {
-      return (day) => (day.date.getDay())
+    days ({ calendarDate, prependDays }) {
+      return (prependDays ? prevMonthDays(calendarDate) : [])
+        .concat(
+          Array.from(Array(calendarDate.daysInMonth()).keys())
+            .map(d => ({ date: createDate(calendarDate.year(), calendarDate.month() + 1, d + 1) }))
+        )
     },
-    templateready () {
-      return typeof this.generatorconfig !== 'function'
+    weekNumbers ({ days }) {
+      return days.map(d => d.date.week())
     },
-    resolvedayselected () {
-      const month = this
-      return (day) => {
-        if (month.isdayselected !== undefined && month.isdayselected(day.row)) return 'selected'
-        return ''
+    keyedDays ({ days }) {
+      return days.map(m => ({ isprev: m.isprev, date: m.date, daykey: m.date.unix() }))
+    },
+    minWeekNumber ({ weekNumbers }) {
+      return Math.min(...weekNumbers)
+    },
+    maxWeekNumber ({ weekNumbers }) {
+      return Math.max(...weekNumbers)
+    },
+    weekRange ({ minWeekNumber, maxWeekNumber, weekNumbers }) {
+      return utils.range(minWeekNumber, maxWeekNumber, 1).filter(w => weekNumbers.indexOf(w) !== -1)
+    },
+
+    checkboxid () {
+      return ({ day }) => `wd_${day}`
+    },
+    isChecked ({ selectedWeekdays }) {
+      return (row) => {
+        return selectedWeekdays.indexOf(row.day) >= 0
       }
     },
-    source () {
-      return this.mode ? this.weekdays : this.calendar
-    },
-    weekdaylist () {
-      const month = this
-      if (month.calendar.length === 0) return () => []
-      return (wd) => {
-        return month.calendar.filter(d => d.date.getDay() === wd.row.id)
+    weekdayClass ({ selectedWeekdays, sixDays }) {
+      return (weekday, index) => {
+        // const isselected = selectedWeekdays.indexOf(weekday.day) >= 0
+        if ((index === 5 && !sixDays) || index === 6) return 'holiday'
+        return [`weekday-${weekday.info.long.toLowerCase()}s`, {
+          'is-selected': selectedWeekdays.indexOf(weekday.day) >= 0,
+          holiday: (index === 5 && !sixDays) || index === 6
+        }]
       }
     },
-    rootrowclass () {
-      const month = this
-      return (row) => ([{ 'weekday-container col': month.mode === 1 }, row.class])
-    }
-  },
-  methods: {
-    onready (calendar) {
-      this.newcalendar = calendar
-    },
-    ondialogclose () {
-      this.selectedweekdays = []
-      this.selecteddays = []
-    },
-    templateselect () {
-      this.runtemplate = true
-    },
-    addday () {
-    },
-    weekdayselect (event, weekday) {
-      const index = this.selectedweekdays.indexOf(weekday.row.id)
-      if (index === -1) this.selectedweekdays.push(weekday.row.id)
-      else this.selectedweekdays.splice(index, 1)
-    },
-    resolveweekdayselected (weekday) {
-      return this.selectedweekdays.indexOf(weekday.row.id) >= 0
-    },
-    closedialog (...args) {
-      this.runtemplate = false
-    },
-    run ({ $event, payload }) {
-      this.createnew(this.newcalendar.filter(payload.daycompare), payload, this.closedialog)
+    isDaySelected ({ selectedDays }) {
+      return ({ row }) => {
+        return selectedDays.findIndex(d => d.date.date() === row.date.date() && d.date.month() === row.date.month()) >= 0
+      }
     }
   }
 }
 </script>
 
 <style>
-  .cd-month {
-
+  .mw-mc {
+    max-width: min-content;
   }
-  .month-header {
-    text-align: center;
-    text-transform: uppercase;
-    font-weight: bold;
-  }
-  .prev-month {
-    opacity: 0.5;
-    pointer-events: none;
-  }
-  .cd-weekend {
-    color: salmon;
+  .cd-weekday--container {
+    max-width: 14%;
   }
   .holiday {
-    color: salmon;
+    color: red;
   }
-  .weekday-name {
-    font-size: 1.5em;
-    user-select: none;
-    text-align: center;
-    font-weight: bold;
+  .cd-day.compact {
+    max-width: 50px;
   }
-  .weekday-container {
-    text-align: center;
-  }
-  .new-day--template {
-    text-align: center;
-  }
-  i {
-    padding-left: 0.5em;
-    padding-right: 0.5em;
-  }
-  .month-no-data {
-    padding-top: 2em;
-    padding-bottom: 2em;
-    margin-bottom: 1em;
-    text-align: center;
-    border: 0.01em solid gray;
-  }
-  .template-selector {
-    width: inherit;
-  }
-  .month-item-wrap.selected {
-    background-color: rgba(117, 115, 115, 0.445);
-    font-weight: bold;
-  }
-  .template-weekday-list {
-    font-size: 1rem;
-    font-weight: 200;
-  }
-  .preview-day-number {
-    padding: 3px;
-  }
-  .commit-template {
-    text-align: right;
-  }
-  .commit-template > .btn {
-    margin-left: 1em;
-  }
-    .compact {
-    margin-bottom: unset!important;
-  }
-  .month-template-preview.compact {
-    font-size: 0.7em;
-  }
-  .compact:hover {
-    box-shadow: none;
-  }
-  .selectable {
-    cursor: pointer;
+  .cd-month.compact {
+    width: 350px;
   }
 </style>
--- src/components/cd-tabs.vue
+++ src/components/cd-tabs.vue
@@ -1,9 +1,47 @@
 <template>
-  <cd-list :collection="tabs" keyfield="tabkey" listclass="nav nav-tabs mp3" :listrole="keyfield" :listitemclicked="ontabselected">
-    <button slot-scope="scope" :ref="tabid(scope.index)" class="nav-link" role="tab" :class="{ 'active': scope.index === current }" :tabindex="scope.index">{{ scope.row[name] }}</button>
-    <div class="tab-pane m-4" slot="footer">
-      <slot></slot>
+  <cd-list class="cd-tabs container" :class="{ 'row': isCol, 'col': isRow}" :collection="tabs" :keyfield="tabKey"
+    :listclass="['cd-tabs--wrap list-unstyled nav nav-tabs border-0 px-0', tabListClass,
+      {
+        'flex-column w-auto': isCol,
+        'd-flex w-100' :isRow,
+        'ps-0': inRight,
+        'pe-0': inLeft,
+      }]" :rowclass="['cd-tab--wrap nav-item', { 'd-flex': isRow }]" listrole="tablist" itemrole="tab">
+    <template v-if="isHeaderContent" slot="header">
+      <div class="cd-tabs--content container tab-content border container-fluid px-2" :class="[{ 'col': isCol }, innerClass.content]">
+        <slot name="content"></slot>
+      </div>
+    </template>
+    <div class="sring-start" :class="{
+      'border-end': inLeft,
+      'border-start': inRight,
+      'border-bottom': inHeader,
+      'border-top': inFooter
+    }" slot="pre">
+      <div :class="{ 'w-1': isRow, 'h-1': isCol }"/>
     </div>
+    <a class="nav-link p-0 cd-tab border-0 mb-0" data-toggle="tab" slot-scope="{ row, index }" :href="`#${row[tabKey]}`">
+      <div v-on:click.capture="onTabSelect($event, row)" class="cd-tab--header p-2 border rounded-0"
+        :class="[row.class, innerClass.rounded, isActive(row) ? ['active', innerClass.activeBorder] : innerClass.border]">
+        <slot :tab="row" :index="index">
+          <span class="cd-tab--header-default">{{ resolveTabCaption(row) }}</span>
+        </slot>
+      </div>
+    </a>
+    <div class="spring-end d-flex p-2" slot="post" :class="{
+      'flex-grow-1': isRow,
+      'border-end': inLeft,
+      'border-start': inRight,
+      'border-bottom': inHeader,
+      'border-top': inFooter
+    }">
+      <div :class="{ 'w-1': isRow, 'h-1': isCol }"/>
+    </div>
+    <template v-if="isFooterContent" slot="footer">
+      <div class="cd-tabs--content container tab-content border container-fluid px-2" :class="[{ 'col': isCol }, innerClass.content]">
+        <slot name="content"></slot>
+      </div>
+    </template>
   </cd-list>
 </template>
 
@@ -15,24 +53,113 @@ export default {
     'cd-list': CDList
   },
   props: {
-    tabs: { type: Array, required: true, description: 'список вкладок' },
-    ontabselected: { type: Function, required: true, description: 'Что произойдёт при выборе вкладки' },
-    keyfield: { type: String, default: 'tabkey', description: 'Свойство-идентификатор вкладки' },
-    current: { type: Number, default: 0, description: 'Выбранный индекс' },
-    name: { type: String, default: 'caption', description: 'Свойство, где находится заголовок вкладки' }
-  },
-  data (tabcontrol) {
-    return {
+    tabListClass: { type: [String, Object, Array], description: 'CSS классы для списка вкладок' },
+    tabs: { type: Array, required: true, description: 'Вкладки' },
+    tabCaption: { type: [Function, String], description: 'Свойство объекта [tab] из массива [tabs], в котором находится заголовок вкладки, или же функция, которая для объекта вкладки возвращает её заголовок' },
+    tabKey: { type: String, required: true, default: 'key', description: 'Свойство объекта из массива [tabs], в котором находится идентификатор вкладки' },
+    isTabDisabled: { type: Function, description: 'Вычисляем для объекта вкладки и её индекса в массиве [tabs], будет ли вкладка задисаблена' },
+    onTabSelect: { type: Function, description: 'Что произойдёт при клике на вкладку' },
+    orientation: {
+      type: String,
+      default: 'row-top',
+      validator: function (value) {
+        return ['row-top', 'row-bottom', 'col-left', 'col-right'].indexOf(value) >= 0
+      },
+      description: 'Расположение списка вкладок'
     }
   },
   computed: {
-    tabid () {
-      return (index) => `tab${index}`
+    resolveTabCaption ({ tabCaption }) {
+      return (tab, index) => tabCaption === undefined
+        ? tab
+        : (typeof tabCaption === 'function' ? tabCaption(tab, index) : tab[tabCaption])
+    },
+    innerClass ({ inLeft, inRight, inHeader, inFooter }) {
+      return {
+        content: {
+          'border-start-0': inLeft,
+          'border-top-0': inHeader,
+          'border-bottom-0': inFooter,
+          'border-end-0': inRight
+        },
+        rounded: {
+          'rounded-start': inLeft,
+          'rounded-end': inRight,
+          'rounded-bottom': inFooter,
+          'rounded-top': inHeader
+        },
+        border: {
+          'border-top': inFooter,
+          'border-end': inLeft,
+          'border-start': inRight,
+          'border-bottom': inHeader
+        },
+        activeBorder: {
+          'border-top-0': inFooter,
+          'border-end-0': inLeft,
+          'border-start-0': inRight,
+          'border-bottom-0': inHeader,
+          'fw-bold': true
+        }
+      }
+    },
+    resolveTabDisabled () {
+      const isTabDisabled = this.isTabDisabled
+      const isFunction = typeof isTabDisabled === 'function'
+      return (row, index) => isTabDisabled === undefined
+        ? false
+        : isFunction
+          ? isTabDisabled(row, index)
+          : isTabDisabled
+    },
+    isActive ({ $route, tabKey }) {
+      return (row) => {
+        return $route.hash === `#${row[tabKey]}`
+      }
+    },
+    inHeader ({ orientation }) {
+      return ['row-top'].indexOf(orientation) >= 0
+    },
+    inFooter ({ orientation }) {
+      return ['row-bottom'].indexOf(orientation) >= 0
+    },
+    isActiveInFooter ({ isActive, inFooter }) {
+      return (row) => isActive(row) && inFooter
+    },
+    isRow ({ orientation }) {
+      return ['row-bottom', 'row-top'].indexOf(orientation) >= 0
+    },
+    isCol ({ orientation }) {
+      return ['col-left', 'col-right'].indexOf(orientation) >= 0
+    },
+    inLeft ({ orientation }) {
+      return ['col-left'].indexOf(orientation) >= 0
+    },
+    inRight ({ orientation }) {
+      return ['col-right'].indexOf(orientation) >= 0
+    },
+    isHeaderContent ({ inFooter, inRight }) {
+      return inFooter || inRight
+    },
+    isFooterContent ({ inHeader, inLeft }) {
+      return inHeader || inLeft
     }
   }
 }
 </script>
 
 <style>
+  .cd-tab--wrap {
+    max-width: min-content;
+  }
 
+  .width-maxc {
+    min-width: max-content;
+  }
+  .h-1 {
+    height: 1vh;
+  }
+  .w-1 {
+    width: 1vw;
+  }
 </style>
