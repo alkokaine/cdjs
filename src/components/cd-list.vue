ba6a7ad3e6f2f15a8670e4f608a58061b313297a
Алексей Коковин <KokovinAA@cross-d.ru>
2023-08-10 21:44:23 +0300

    резня

--- src/components/cd-list.vue
+++ src/components/cd-list.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="cd-list" v-on:mouseleave="listleave">
+  <div class="cd-list">
     <slot name="header"></slot>
-    <ul v-if="showitems" v-loading="isloading" :role="listrole" class="cd-list--wrap" :class="[listclass, { 'inner': inner }]">
+    <ul v-loading="isloading" :role="listrole" class="cd-list--wrap" :class="[listclass, { 'inner': inner, 'd-none': showitems }]">
       <li v-if="isempty" class="cd-list--item no-data">
         <slot name="no-data"></slot>
       </li>
       <li v-for="(row, index) in filtered"
         :key="rowkey(row)" class="cd-list--item" :class="rowClassResolved(row, index)"
-        v-on:click="listitemclicked($event, { row, index })"
-        v-on:mouseenter="listitementered($event, { row, index })"
         role="presentation">
         <slot :row="row" :index="index"></slot>
       </li>
@@ -35,11 +33,6 @@ export default {
      * строка или функция, возвращающая css-класс для для
      */
     listclass: { type: [String, Array], description: 'Строка или массив, содержащие названия классов, которые будут применены к ul внутри cd-list' },
-    listitemclicked: { type: Function, default: function (event, scope) {}, description: 'Функция, которая выполнится по клику на элементе списка' },
-    listitementered: { type: Function, default: function (event, scope) {}, description: 'Функция, которая выполнится при входе мыши на элемент списка' },
-    listitemleaved: { type: Function, default: function (event, scope) {}, description: 'Функция, которая выполнится при выходе из элемента списка' },
-    listleave: { type: Function, default: function (event) {}, description: 'Функция, которая выполнится при выходе мыши из списка' },
-    listenter: { type: Function, default: function (event) {}, description: 'Функция, которая выполнится при входе мыши в список' },
     /**
      * свойство-признак того, что эта коллекция вложена в другую
      */
