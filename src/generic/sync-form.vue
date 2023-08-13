9abea19852cb98bf50bfc0493eb87d0cc8b7db87
alkokaine <alexey.kokovin@yandex.ru>
2023-08-13 21:12:25 +0300

    sync form test

---
+++ src/generic/sync-form.vue
@@ -0,0 +1,33 @@
+<template>
+  <cd-list :collection="values" keyfield="key" listclass="list-unstyled d-flex" rowclass="w-50 m-3">
+    <cd-form slot-scope="{ row }" :id="row.key" :descriptor="row.descriptor" :payload="row.payload" :sync="row.sync" :showcontrols="!row.sync"
+    :submit="onSubmit(row)" :reset="onReset(row)">
+      <div slot="header" slot-scope="{ model }">{{ model }}</div>
+      <div slot="footer" class="payload-value p-3">{{ row.payload }}</div>
+    </cd-form>
+  </cd-list>
+</template>
+<script>
+import CDList from '../components/cd-list.vue'
+import CDForm from '../components/cd-form.vue'
+export default {
+  name: 'sync-form',
+  components: {
+    'cd-list': CDList,
+    'cd-form': CDForm
+  },
+  props: {
+    values: { type: Array, required: true },
+    submit: { type: Function },
+    reset: { type: Function }
+  },
+  computed: {
+    onSubmit ({ submit }) {
+      return (row) => submit(row)
+    },
+    onReset ({ reset }) {
+      return (row) => reset(row)
+    }
+  }
+}
+</script>
