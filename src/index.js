de2de38c6b73495036676d45760951628af35976
Алексей Коковин <KokovinAA@cross-d.ru>
2023-09-15 12:13:39 +0300

    cd-day, cd-day-select

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import CDGrid from './components/cd-grid.vue'
 import CDList from './components/cd-list.vue'
 import CDMenu from './components/cd-menu.vue'
 import CDMonth from './components/cd-month.vue'
+import CDDaySelect from './components/cd-day-select.vue'
+import CDDay from './components/cd-day.vue'
 import CDTabs from './components/cd-tabs.vue'
 import CDSettingContainer from './components/cd-setting-container.vue'
 import CDProps from './components/cd-props.vue'
@@ -14,6 +16,8 @@ export default {
   CDMenu,
   CDTabs,
   CDMonth,
+  CDDay,
+  CDDaySelect,
   CDProps,
   CDSettingContainer
 }
